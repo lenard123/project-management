1cbfa13d79ec8b4d295620ec0653e24f865b9189
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2023-07-01 06:17:58 +0000

    WIP

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -24,14 +24,13 @@ const links = reactive([
             </v-btn>
         </v-toolbar>
         <v-navigation-drawer temporary app class="bg-indigo" v-model="drawer">
+            <v-row justify="center" class="mt-5">
+                <v-avatar size="100">
+                    <img src="/avatar-1.png" />
+                </v-avatar>
+            </v-row>
             <v-list>
-                <v-list-item
-                    v-for="link in links" 
-                    :prepend-icon="`${link.icon}`"
-                    :key="link.route" 
-                    route
-                    :to="link.route"
-                    >
+                <v-list-item v-for="link in links" :prepend-icon="`${link.icon}`" :key="link.route" route :to="link.route">
                     {{ link.title }}
                 </v-list-item>
             </v-list>
